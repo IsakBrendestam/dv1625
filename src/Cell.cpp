0c58609e075dc6fe4914cba9b58fc9cd1745c988
IsakBrendestam <isak.kruger01@gmail.com>
2023-09-15 15:43:37 +0200

    Added hovering over cells

--- src/Cell.cpp
+++ src/Cell.cpp
@@ -10,6 +10,14 @@ Cell::Cell(int xPos, int yPos, int width, int height, const SDL_Color color)
     m_color = color;
 }
 
+bool Cell::Hover(int mouseX, int mouseY)
+{
+    if (mouseX > m_rect.x && mouseX < m_rect.x + m_rect.w &&
+        mouseY > m_rect.y && mouseY < m_rect.y + m_rect.h)
+        return true;
+    return false;
+}
+
 void Cell::Draw(SDL_Renderer* renderer)
 {
     SDL_SetRenderDrawColor(renderer, m_color.r, m_color.g, m_color.b, m_color.a);
